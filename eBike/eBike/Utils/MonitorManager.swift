75060eb16d7e79c8876efe408e64b2765e35e18d
cankurtur <cankurtur@gmail.com>
2023-04-21 02:38:51 +0300

    create monitor manager

---
+++ eBike/eBike/Utils/MonitorManager.swift
@@ -0,0 +1,35 @@
+//
+//  MonitorManager.swift
+//  eBike
+//
+//  Created by Can Kurtur on 21.04.2023.
+//
+
+import Network
+
+final class MonitorManager {
+   
+    static let shared: MonitorManager = MonitorManager()
+    private let monitor: NWPathMonitor = NWPathMonitor()
+    private var status: NWPath.Status = .satisfied
+    var isReachable: Bool { status == .satisfied }
+    
+    private init() { }
+}
+
+extension MonitorManager {
+    func startMonitoring() {
+        monitor.pathUpdateHandler = {[weak self] monitorPath in
+            guard let self = self else { return }
+            
+            self.status = monitorPath.status
+        }
+        
+        let queue = DispatchQueue(label: "NetworkMonitor")
+        monitor.start(queue: queue)
+    }
+    
+    func cancel() {
+        monitor.cancel()
+    }
+}
