986f84cc8753077b96b59d86c018513477022a2e
Can <51170501+cankurtur@users.noreply.github.com>
2023-04-21 18:09:26 +0300

    Feature/main module (#7)
    * add new resources
    
    * change direction of base module
    
    * create main module

---
+++ eBike/eBike/Module/Main/MainRouter.swift
@@ -0,0 +1,82 @@
+//
+//  MainRouter.swift
+//  eBike
+//
+//  Created by Can Kurtur on 21.04.2023.
+//
+
+
+import UIKit
+
+// MARK: - Constant
+
+private enum Constant {
+    struct TabBarItem {
+        static let titleFont: UIFont = FontFamily.CircularSpotifyText.bold.font(size: 12)
+        static let selectedTitleColor: UIColor = Colors.appGreen.color
+        static let unSelectedTitleColor: UIColor = Colors.appGray.color
+        
+        static let mapTabImage: UIImage = UIImage(systemName: "map.circle")!.withRenderingMode(.alwaysTemplate)
+        static let mapTitle: String = L10n.TabBarItems.map
+    }
+}
+
+// MARK: - RouterInterface
+
+protocol MainRouterInterface: RouterInterface {
+    func getViewControllers() -> [UINavigationController]
+}
+
+// MARK: - MainRouter
+
+final class MainRouter {
+    private weak var presenter: MainPresenter?
+    private var navigationController: UINavigationController?
+
+    init(navigationController: UINavigationController?) {
+        self.navigationController = navigationController
+    }
+    
+    static func createModule(tabType: TabType = .map) -> UIViewController {
+
+        let interactor = MainInteractor()
+        let view = MainViewController()
+        let navCon = UINavigationController(rootViewController: view)
+        navCon.modalPresentationStyle = .fullScreen
+        let router = MainRouter(navigationController: navCon)
+        let presenter = MainPresenter(router: router, interactor: interactor, view: view, tabType: tabType)
+        router.presenter = presenter
+        view.presenter = presenter
+        interactor.output = presenter
+        presenter.setupViewControllers()
+        return navCon
+    }
+}
+
+// MARK: - MainRouterInterface
+
+extension MainRouter: MainRouterInterface {
+    func getViewControllers() -> [UINavigationController] {
+        let selectedTitleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: Constant.TabBarItem.selectedTitleColor,
+            NSAttributedString.Key.font: Constant.TabBarItem.titleFont
+        ]
+        
+        let unselectedTitleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: Constant.TabBarItem.unSelectedTitleColor,
+            NSAttributedString.Key.font: Constant.TabBarItem.titleFont
+        ]
+
+        let mapView = UINavigationController(rootViewController: ViewController())
+        mapView.tabBarItem.title = Constant.TabBarItem.mapTitle
+        mapView.tabBarItem?.image = Constant.TabBarItem.mapTabImage
+        mapView.tabBarItem?.setTitleTextAttributes(selectedTitleTextAttributes, for: .selected)
+        mapView.tabBarItem?.setTitleTextAttributes(unselectedTitleTextAttributes, for: .normal)
+
+        return [
+            mapView
+        ]
+    }
+}
+
+
---
+++ eBike/eBike/Module/Main/TabType.swift
@@ -0,0 +1,12 @@
+//
+//  TabType.swift
+//  eBike
+//
+//  Created by Can Kurtur on 21.04.2023.
+//
+
+import Foundation
+
+enum TabType: Int {
+    case map
+}
