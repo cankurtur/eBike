310f33c4d6b7d2e2154f5475d034def124188629
Can <51170501+cankurtur@users.noreply.github.com>
2023-04-23 20:14:11 +0300

    Feature/app-popup (#13)
    * create apppopupview
    
    * create nibview
    
    * change swift message configurations

--- eBike/eBike/Network/Extensions/UIViewExtension.swift
+++ eBike/eBike/Network/Extensions/UIViewExtension.swift
@@ -64,4 +64,11 @@ extension UIView {
             }
         }
     }
+    
+    func loadNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nibName = type(of: self).description().components(separatedBy: ".").last!
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as! UIView
+    }
 }
