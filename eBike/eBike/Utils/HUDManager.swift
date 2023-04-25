96b469684f2a4250d556d1f30d0c3f2d9b24b496
cankurtur <cankurtur@gmail.com>
2023-04-25 21:33:44 +0300

    create hud manager

--- eBike/eBike/Utils/HUDManager.swift
+++ eBike/eBike/Utils/HUDManager.swift
@@ -5,4 +5,35 @@
 //  Created by Can Kurtur on 25.04.2023.
 //
 
-import Foundation
+import MBProgressHUD
+
+// MARK: - HUDManager
+
+final class HUDManager {
+    
+    static let shared: HUDManager = HUDManager()
+    
+    private init() {}
+    
+    func showHUD(text: String, onMainThread: Bool = false, viewController: UIViewController) {
+        if onMainThread {
+            let progressHUD = MBProgressHUD.showAdded(to: viewController.view, animated: true)
+            progressHUD.mode = .indeterminate
+            progressHUD.label.text = text
+            progressHUD.label.font = UIFont.systemFont(ofSize: 14)
+        } else {
+            DispatchQueue.main.async {
+                let progressHUD = MBProgressHUD.showAdded(to: viewController.view, animated: true)
+                progressHUD.mode = .indeterminate
+                progressHUD.label.text = text
+                progressHUD.label.font = UIFont.systemFont(ofSize: 14)
+            }
+        }
+    }
+
+    func dismissHUD(isAnimated: Bool = true, viewController: UIViewController) {
+        DispatchQueue.main.async {
+            MBProgressHUD.hide(for: viewController.view, animated: isAnimated)
+        }
+    }
+}
