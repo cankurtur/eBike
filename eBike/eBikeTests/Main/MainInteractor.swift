b4ae76bafe7023fdf50b17db81d7df71a0d63ff6
cankurtur <cankurtur@gmail.com>
2023-04-27 21:37:26 +0300

    write unit test for main module

---
+++ eBike/eBikeTests/Main/MainInteractor.swift
@@ -0,0 +1,14 @@
+//
+//  MainInteractor.swift
+//  eBikeTests
+//
+//  Created by Can Kurtur on 27.04.2023.
+//
+
+@testable import eBike
+
+final class MockMainInteractor: MainInteractorInterface {
+    
+    weak var output: MainInteractorOutput?
+    
+}
